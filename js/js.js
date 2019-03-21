eabd49ef7390b28a0374776649e8f0cda80f01b9
dadanilov <48787227+dadanilov@users.noreply.github.com>
2019-03-22 00:21:54 +0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -75,3 +75,12 @@ var StickyElement = function(node){
 };
 
 var demo = new StickyElement($('#sticky'));
+
+
+
+	$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+
+	$("#menu-icon").on("click", function() {
+	  $("nav").slideToggle();
+	  $(this).toggleClass("active");
+	});
